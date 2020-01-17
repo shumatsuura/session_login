fe07b8519c652159cfd0a91c30158a7896288e43
shumatsuura <shunsuke.mtr@gmail.com>
2020-01-17 11:38:30 +0900

    start assorciation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  before_validation { email.downcase! }
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+
+  validates :name, presence: true, length: { maximum:30}
+  validates :email, presence: true, length: { maximum:255}
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :email, uniqueness: true
 end
